04c3a367276da8e840d37a8cde258f6d66b37956
IPlayOften <logantpeterson2@gmail.com>
2022-07-03 17:07:38 -0400

    various settings changes

--- src/main/java/com/helmetcheck/HelmetCheckPlugin.java
+++ src/main/java/com/helmetcheck/HelmetCheckPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.helmetcheck;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,26 +14,26 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Helmet Check"
 )
-public class ExamplePlugin extends Plugin
+public class HelmetCheckPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private HelmetCheckConfig config;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
+		log.info("Helmet Check started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		log.info("Helmet Check stopped!");
 	}
 
 	@Subscribe
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	HelmetCheckConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(HelmetCheckConfig.class);
 	}
 }
